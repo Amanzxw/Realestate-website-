885d992c20c237c0aecf934030bc2fef0a1ac5e7
Amandeep kaur <157288021+Amanzxw@users.noreply.github.com>
2024-02-19 11:28:58 +0530

    Add files via upload

---
+++ realestate.js
@@ -0,0 +1,10 @@
+
+
+document.getElementById("nav-button").addEventListener("click", function() {
+    var navMenu = document.getElementById("nav-menu");
+    if (navMenu.style.display === "flex") {
+        navMenu.style.display = "none";
+    } else {
+        navMenu.style.display = "flex";
+    }
+});
